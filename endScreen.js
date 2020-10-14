41edd5e24b8e32a36ae9a2419c8596c4a3fd9900
shalomjlee <shalomjlee@gmail.com>
2020-10-13 19:19:01 -0500

    readme stuff

--- endScreen.js
+++ endScreen.js
@@ -1,5 +1,5 @@
 
-const maxQuestions = 10;
+
 const finalScore = (document.getElementById('finalScore'))
 const newScore = localStorage.getItem('newScore')
-finalScore.innerHTML =(`Congratulations you got ${newScore}/${maxQuestions} right!`);
+finalScore.innerHTML =(`Congratulations you got ${newScore}/10 right!`);
